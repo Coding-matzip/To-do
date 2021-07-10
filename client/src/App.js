0525d5d8a688d57632f2864781dd6d40fc9575ed
Sangyun Song <benfsong11@gmail.com>
2021-07-10 15:53:07 +0900

    로그아웃 기능 연동 완료

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 import { Link, Route } from "react-router-dom";
 import LandingPage from "./Component/Pages/LandingPage/LandingPage";
 import LoginPage from "./Component/Pages/MemberPage/LoginPage";
@@ -11,10 +11,17 @@ import homeIcon from "./image/icon-main.svg";
 import scheduleIcon from "./image/icon-check-last-schedule.svg";
 import Auth from "./hoc/auth";
 import { BrowserRouter as Router, Switch } from "react-router-dom";
+import "./mediaquery.css";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import axios from "axios";
+import { withRouter } from "react-router-dom";
 import PrivateRoute from "./lib/PrivateRoute";
 import PublicRoute from "./lib/PublicRoute";
 
-function App() {
+const { Kakao } = window;
+
+function App(props) {
   const buttonActive = (event) => {
     let menuList = document.querySelectorAll("#menu li");
     menuList.forEach((value, index, array) => {
@@ -24,12 +31,50 @@ function App() {
     event.currentTarget.classList.add("active");
   };
 
+  const [isLogin, setIsLogin] = useState(false); // 로그인을 했는지 여부
+
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    console.log("handleClose true");
+    setAnchorEl(null);
+  };
+
+  const onClickHandler = () => {
+    axios.get(`/api/users/logout`).then((response) => {
+      if (response.data.success) {
+        props.history.push("/login");
+      } else {
+        alert("로그아웃 실패");
+      }
+    });
+  };
+
+  const logoutWithKakao = () => {
+    if (Kakao.Auth.getAccessToken()) {
+      console.log(
+        "카카오 인증 액세스 토큰이 존재합니다.",
+        Kakao.Auth.getAccessToken()
+      );
+      Kakao.Auth.logout(() => {
+        console.log("로그아웃되었습니다.", Kakao.Auth.getAccessToken());
+        setIsLogin(false);
+        localStorage.clear();
+        props.history.push("/login");
+      });
+    }
+  };
+
   return (
     <Router>
       <div className="App">
         <div id="content-box">
           <Switch>
-            <Route exact path="/" component={Auth(LandingPage, null)}></Route>
+            <Route exact path="/" component={Auth(LandingPage, true)}></Route>
             <Route
               exact
               path="/login"
@@ -54,11 +99,37 @@ function App() {
         </div>
         <nav>
           <ul id="menu">
-            <li onClick={buttonActive} key="logout">
-              <Link to="/login">
+            {/* 비로그인시 */}
+            {isLogin && (
+              <li onClick={buttonActive} key="logout">
+                <Link to="/login">
+                  <img src={logoutIcon} alt="logout" />
+                </Link>
+              </li>
+            )}
+            {/* 로그인시 */}
+            {!isLogin && (
+              <li
+                onClick={(buttonActive, handleClick)}
+                aria-controls="simple-menu"
+                aria-haspopup="true"
+                key="logout"
+              >
                 <img src={logoutIcon} alt="logout" />
-              </Link>
-            </li>
+              </li>
+            )}
+            {/* https://material-ui.com/components/menus/ */}
+            <Menu
+              id="simple-menu"
+              anchorEl={anchorEl}
+              keepMounted
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              <div id="member_info_email">example@example.com</div>
+              <MenuItem onClick={onClickHandler}>로그아웃</MenuItem>
+              <MenuItem onClick={logoutWithKakao}>카카오 로그아웃</MenuItem>
+            </Menu>
             <li className="active" onClick={buttonActive} key="home">
               <Link to="/">
                 <img src={homeIcon} alt="home" />
@@ -76,4 +147,4 @@ function App() {
   );
 }
 
-export default App;
+export default withRouter(App);
